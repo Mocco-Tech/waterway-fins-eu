52f46db002b3b63f21608ad7cc10fe5892535272
davidlyashuk <info@moccotech.com>
2024-11-15 14:52:44 +0000

    Updated footer icons errors

--- src/components/custom/footer/FooterBottom.tsx
+++ src/components/custom/footer/FooterBottom.tsx
@@ -6,17 +6,12 @@ export default function FooterBottom() {
   const year = new Date().getFullYear();
 
   return (
-    <div className="flex justify-between items-center">
+    <div className="flex flex-col items-start gap-4 md:flex-row justify-between md:items-center">
       <div className="flex gap-10 flex-col md:flex-row flex-wrap text-sm font-medium text-slate-600">
         <p className="order-2 md:order-1">
           © {year} Waterway Fins Group Ltd. All rights reserved
         </p>
         <ul className="order-1 md:order-2 flex flex-col md:flex-row gap-6">
-          {/* <li>
-          <Link href="/guides" className="hover:text-primary">
-            Guides
-          </Link>
-        </li> */}
           <li>
             <Link href="/terms-and-conditions" className="hover:text-primary">
               Terms & Conditions
@@ -27,11 +22,6 @@ export default function FooterBottom() {
               Privacy Policy
             </Link>
           </li>
-          {/* <li>
-          <Link href="/company-details" className="hover:text-primary">
-            Company Details
-          </Link>
-        </li> */}
         </ul>
       </div>
       <PaymentIconsGrid />
