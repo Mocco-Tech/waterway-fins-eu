c5b432f6c729f55f133071df474ea4ee535e2944
David Lyashuk <info@moccotech.com>
2024-11-16 19:52:33 +0200

    Fixed styling issue

--- src/components/custom/footer/Footer.tsx
+++ src/components/custom/footer/Footer.tsx
@@ -4,7 +4,7 @@ import FooterBottom from "./FooterBottom";
 
 export default function Footer() {
   return (
-    <footer className="max-w-screen-2xl lg:mx-auto border-t border-border mx-4 md:mx-10 py-20">
+    <footer className="max-w-screen-2xl mx-auto lg:mx-auto border-t border-border px-4 md:px-10 py-20">
       <FooterTop />
       <FooterBottom />
     </footer>
